a5e836bbd8babdcee111f4f2a4ce4deee7cca1ba
zisheng7777 <raphael89918@gmail.com>
2024-12-19 17:56:15 +0800

    add loop rate

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -5,11 +5,14 @@ int main(int argc,char **argv)
     rclcpp::init(argc, argv);
 
     auto node = rclcpp::Node::make_shared("test_node");  
-
+    rclcpp::WallRate loop_rate(1);
     Test test;
     while(rclcpp::ok())
     {
+        rclcpp::spin_some(node);
         test.print("hello!!!");
+        loop_rate.sleep();
     }
+    rclcpp::shutdown();
     return 0;
 }
